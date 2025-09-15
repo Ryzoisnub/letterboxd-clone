353dc9b95f12a96ef848132e32960848fecac7bd
AbhijithPM <161154212+AbhijithPM507@users.noreply.github.com>
2025-09-15 23:02:01 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import Home from './pages/Home'
 import MovieDetails from './pages/MovieDetails'
 import Profile from './pages/Profile'
 import Search from './pages/Search'
-
+//MYRR
 function App() {
   return (
     <Router>
